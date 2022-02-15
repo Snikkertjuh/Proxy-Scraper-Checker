34b3c0ce8262161e3a0884954df312541164e622
Snikker <snikker.zakelijk@protonmail.com>
2022-02-15 10:11:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 import requests, os, threading
 
-def main():
-    os.system("cls")
-    print(" > Proxy Scraper & Checker \n")
-    url = "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks5&timeout=5000&country=all"
-    open(f"{os.getenv('TEMP')}\\proxies.txt", "wb").write(requests.get(url).content)
-    with open("proxies.txt", "w") as file:
-        proxies = open(f"{os.getenv('TEMP')}\\proxies.txt", "r")
-        for i in range(10): threading.Thread().start()
-        for line in proxies:
-            proxy = line.strip("\n")
-            proxydict = {
-                "https": f"socks5://{proxy}"
-            }
-            try:
-                request = requests.get("https://api.ipify.org", proxies=proxydict, timeout=3)
-                print(request.text)
-                file.write(proxy)
-            except Exception:
-                pass
-
-if __name__ == "__main__":
-    main()
+os.system("cls")
+print(" > Proxy Scraper & Checker \n")
+url = "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks5&timeout=5000&country=all"
+open(f"{os.getenv('TEMP')}\\proxies.txt", "wb").write(requests.get(url).content)
+file = open("proxies.txt", "w")
+proxies = open(f"{os.getenv('TEMP')}\\proxies.txt", "r")
+for i in range(50): threading.Thread().start()
+for line in proxies:
+    proxy = line.strip("\n")
+    proxydict = {
+        "https": f"socks5://{proxy}"
+    }
+    try:
+        request = requests.get("https://api.ipify.org", proxies=proxydict, timeout=3)
+        print(f" > Not working > {request.text}")
+        file.write(proxy)
+    except Exception:
+        print(f" > Not working > {proxy}")
+        pass
